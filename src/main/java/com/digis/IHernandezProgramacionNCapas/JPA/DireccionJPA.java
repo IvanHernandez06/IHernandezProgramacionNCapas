ee7d0a2c43933a1ae2e71a24344c4a03b7febed8
digis <digis@ALIEN6>
2025-08-29 18:51:17 -0600

    Act

--- src/main/java/com/digis/IHernandezProgramacionNCapas/JPA/DireccionJPA.java
+++ src/main/java/com/digis/IHernandezProgramacionNCapas/JPA/DireccionJPA.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-@Entity
+@Entity(name = "Direccion")
 public class DireccionJPA {
 
     @Id
